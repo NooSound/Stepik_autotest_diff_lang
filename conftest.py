ed46373f099daaecd90d34ab095dc33770b5c854
NooSound <al.kremnev@gmail.com>
2022-04-12 16:32:25 +0300

    добавлен time для визуального осмотра добавлен assert

--- conftest.py
+++ conftest.py
@@ -1,25 +1,26 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 
 
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",
+    parser.addoption('--language', action='store',
                      help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-
+    #указываем язык браузера с помощью WebDriver
     user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
 
-
     yield browser
+    time.sleep(2)  #визуальная проверка изменения языка
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -6,4 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_should_see_login_link(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    browser.find_element_by_xpath("//*[@class='btn btn-lg btn-primary btn-add-to-basket']")
+    browser.implicitly_wait(5)
+    assert browser.find_element_by_xpath("//*[@class='btn btn-lg btn-primary btn-add-to-basket']"), "warning, no button!"
